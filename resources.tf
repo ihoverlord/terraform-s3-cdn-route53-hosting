b3a0afa02affd5784ba17dad3b93e7f2ac5dbe42
Himakar Reddy <himakar.reddy@pluto.tv>
2023-01-08 23:09:28 -0400

    s3 bucket policy to allow GetObject only to CDN added

--- resources.tf
+++ resources.tf
@@ -10,3 +10,23 @@ resource "aws_s3_bucket_acl" "s3_bucket_private_acl" {
   bucket = aws_s3_bucket.S3Bucket.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.S3Bucket.id
+  policy = <<EOF
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "PolicyForCloudFront",
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "cloudfront.amazonaws.com"
+                        },
+                        "Action": "s3:GetObject",
+                        "Resource": "${aws_s3_bucket.example.arn}/*"
+                    }
+                ]
+            }
+            EOF
+}
