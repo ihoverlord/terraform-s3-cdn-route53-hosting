75acea4a41e46e38cdc7116cb576fa641b5ca718
Himakar Reddy <himakar.reddy@pluto.tv>
2023-01-08 23:08:08 -0400

    s3 bucket acl private policy added

--- resources.tf
+++ resources.tf
@@ -5,3 +5,8 @@ resource "aws_s3_bucket" "S3Bucket" {
   }
   prevent_destory = false
 }
+
+resource "aws_s3_bucket_acl" "s3_bucket_private_acl" {
+  bucket = aws_s3_bucket.S3Bucket.id
+  acl    = "private"
+}
