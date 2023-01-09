e1b3627c34245893b57d9b5d39d328fe2024ade1
Himakar Reddy <himakar.reddy@pluto.tv>
2023-01-08 23:06:05 -0400

    route53 record name variable created

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "s3_bucket_name_for_hosting" {
   description = "S3 bucket name for hosting code artifacts"
   default     = "s3_bucket_for_hosting_with_tf"
 }
+
+variable "aws_route53_record_name" {
+  type        = string
+  description = "Route53 record name"
+  default     = "dev.example.com"
+}
